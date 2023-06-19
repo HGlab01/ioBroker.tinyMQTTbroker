36ffc809000ee7ee2170b60711473ef5e6dfef8b
HGlab <54232526+HGlab01@users.noreply.github.com>
2023-06-19 19:25:45 +0200

    add NedbPersistence support

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,10 @@ import Aedes from 'aedes';
 import { createServer } from 'aedes-server-factory';
 
 class Tinymqttbroker extends utils.Adapter {
-	aedes: Aedes;
-	server: any;
+	aedes!: Aedes;
+	server!: any;
+	withDB!: boolean;
+	serverPort!: number;
 
 	public constructor(options: Partial<utils.AdapterOptions> = {}) {
 		super({
@@ -22,18 +24,31 @@ class Tinymqttbroker extends utils.Adapter {
 		// this.on('objectChange', this.onObjectChange.bind(this));
 		// this.on('message', this.onMessage.bind(this));
 		this.on('unload', this.onUnload.bind(this));
-		this.aedes = new Aedes();
-		this.aedes.id = 'iobroker_mqtt_broker_' + Math.floor(Math.random() * 100000 + 100000);
-		this.server = createServer(this.aedes);
 	}
 
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	private async onReady(): Promise<void> {
-		const port: number = this.config.option1;
-		this.server.listen(port, () => {
-			this.log.info('MQTT-broker says: Server ' + this.aedes.id + ' started and listening on port ' + port);
+
+		this.serverPort = this.config.option1;
+		this.withDB = this.config.option2;
+
+		if (this.withDB) {
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const NedbPersistence = require('aedes-persistence-nedb');
+			const db = new NedbPersistence({ path: `${utils.getAbsoluteInstanceDataDir(this)}/mqttData`, prefix: '' });
+			this.aedes = new Aedes({ persistence: db });
+		}
+		else {
+			this.aedes = new Aedes();
+		}
+
+		this.aedes.id = 'iobroker_mqtt_broker_' + Math.floor(Math.random() * 100000 + 100000);
+		this.server = createServer(this.aedes);
+
+		this.server.listen(this.serverPort, () => {
+			this.log.info('MQTT-broker says: Server ' + this.aedes.id + ' started and listening on port ' + this.serverPort);
 		})
 		// emitted when a client connects to the broker
 		this.aedes.on('client', (client) => {
