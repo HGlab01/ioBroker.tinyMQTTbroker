f47bfbc9019835d9a4488ab0063718a85f24301f
HGlab <54232526+HGlab01@users.noreply.github.com>
2024-08-21 16:22:36 +0200

    final

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,31 @@
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends('eslint:recommended'), {
-    files: [
-        "**/*.ts",
-        "**/*.cts",
-        "**.*.mts"
-    ],
-    ignores: ["**/main.js"],
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.mocha
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['**/*.js'],
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            indent: ['warn', 4, {
+                SwitchCase: 1
+            }],
+            'no-console': 'off',
+            'no-trailing-spaces': 'error',
+            quotes: ['error', 'single', {
+                avoidEscape: true,
+                allowTemplateLiterals: true
+            }],
+            semi: ['error', 'always'],
+            'comma-dangle': ['error', 'only-multiline']
         },
-        ecmaVersion: 2023,
-        sourceType: 'module'
-    },
-    rules: {
-        indent: ['warn', 4, {
-            SwitchCase: 1
-        }],
-        'no-console': 'off',
-        'no-trailing-spaces': 'error',
-        quotes: ['error', 'single', {
-            avoidEscape: true,
-            allowTemplateLiterals: true
-        }],
-        semi: ['error', 'always'],
-        'comma-dangle': ['error', 'only-multiline']
-    },
-}];
+        languageOptions: {
+            ecmaVersion: 2023,
+            sourceType: 'module'
+        }
+    }
+);
